ac00b07870977cb89a448e92779bd64533384f72
Shantell Moore <moores1@hawkmail.newpaltz.edu>
2019-04-01 20:02:46 -0400

    new editions from break

--- Server/Controllers/users.js
+++ Server/Controllers/users.js
@@ -1,16 +1,32 @@
 const express = require('express');
-const conn = require('../models/mysql_connection');
+const user = require('../models/user');
 
 const app = express.Router();
 
-app.get("/", (req, res) => {
-
-    conn.query("SELECT * FROM 2019Spring_Persons", (err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/", async (req, res, next) => {
+    user.getAll()
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
-
+app.get("/:id", (req, res, next) => {
+    user.get(req.params.id)
+    .then(x=>  res.send(x) )
+    .catch(next)
+});
+app.post("/", (req, res, next) => {
+    user.add(req.body)
+    .then(x=>  res.send(x) )
+    .catch(next)
+});
+app.post("/login", (req, res, next) => {
+    user.login(req.body.email, req.body.password)
+    .then(x=>  res.send(x) )
+    .catch(next)
+});
+app.post("/changePassword", (req, res, next) => {
+    user.changePassword(req.body.email, req.body.oldPassword, req.body.newPassword)
+    .then(x=>  res.send(x) )
+    .catch(next)
+});
 
 module.exports = app;
