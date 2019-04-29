c5056c30c3bcf241b234b4ec16b1090072bda53a
Shantell Moore <moores1@hawkmail.newpaltz.edu>
2019-04-29 13:23:43 -0400

    adding layout for min about you page

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
             <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
             <router-link class="navbar-brand" to="/">
-                <img alt="Vue logo" src="../assets/logo.png" height="30"></router-link>
+                <img alt="Vue logo" src="../assets/logo.png" height="40"></router-link>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
--- src/views/About.vue
+++ src/views/About.vue
@@ -52,13 +52,18 @@ export default {
              Sleep
             </a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>              My Friends
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
 
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4"><div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">{{name}}'s Page</h1>
+    <h1 v-if="Globals.user"> {{Globals.user.name}}'s Page </h1>
         <div class="btn-toolbar mb-2 mb-md-0">
           <div class="btn-group mr-2">
             <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
@@ -88,28 +93,28 @@ export default {
               <td>400</td>
             </tr>
             <tr>
-              <td>1-1-19</td>
-              <td>Running</td>
-              <td>45mins</td>
-              <td>400</td>
+              <td>1-2-19</td>
+              <td>Soccer</td>
+              <td>43mins</td>
+              <td>425</td>
             </tr>
             <tr>
-              <td>1-1-19</td>
-              <td>Running</td>
-              <td>45mins</td>
-              <td>400</td>
+              <td>1-3-19</td>
+              <td>Swimming</td>
+              <td>30mins</td>
+              <td>276</td>
             </tr>
             <tr>
-              <td>1-1-19</td>
+              <td>1-5-19</td>
               <td>Running</td>
-              <td>45mins</td>
-              <td>400</td>
+              <td>37mins</td>
+              <td>363</td>
             </tr>
             <tr>
-             <td>1-1-19</td>
-              <td>Running</td>
-              <td>45mins</td>
-              <td>400</td>
+             <td>1-11-19</td>
+              <td>Hiking</td>
+              <td>62mins</td>
+              <td>687</td>
             </tr>
           </tbody>
         </table>
@@ -119,12 +124,19 @@ export default {
 
 </template>
 <script>
+
+import {Globals} from "@/models/api";
+import { GetFriends } from "@/models/users.js";
 export default {
-  data: () => ({
-    name: "Shantell",
-    age: "23"
-  })
+    data: () => ({
+        Globals: Globals,
+        friends: []
+    }),
+    async mounted(){
+        this.friends = await GetFriends();
+    }
 }
 </script>
 
-<style type="scss">
+<style>
+</style>
