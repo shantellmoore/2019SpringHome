40996b5bd3d02adfff650bce74c3eb89351d7593
Shantell Moore <moores1@hawkmail.newpaltz.edu>
2019-05-13 11:51:13 -0400

    In class final

--- src/views/About.vue
+++ src/views/About.vue
@@ -169,7 +169,7 @@ export default {
     }),
     async mounted(){
         this.friends = await GetFriends();
-        this.workouts = await GetWorkouts
+        this.workouts = await GetWorkouts();
     }
 }
 </script>
